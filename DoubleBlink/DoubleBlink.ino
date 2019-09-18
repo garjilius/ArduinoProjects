d66b638cd22e1f03a24b9132be0b7f1ff84c9814
garjilius <emanuelegar@gmail.com>
2019-09-18 22:30:02 +0200

    Funzione blink in DoubleBlink

--- DoubleBlink/DoubleBlink.ino
+++ DoubleBlink/DoubleBlink.ino
@@ -6,6 +6,7 @@ Quando l'attesa arriva a 5s, si resetta
 #define LED_BUILTIN 13
 #define RED_LED 3
 #define YELLOW_LED 9
+#define restartdelay 5000
 
 int wait;
 // the setup function runs once when you press reset or power the board
@@ -20,19 +21,19 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+  wait +=1000;
 
-    if(wait >= 5000) {
+    if(wait > 5000) {
         wait = 0;
         Serial.println("Ho Resettato il contatore. Tra 5s si riparte!");
-        digitalWrite(LED_BUILTIN,HIGH);
+        //digitalWrite(LED_BUILTIN,HIGH);
+        blink(LED_BUILTIN, 250,restartdelay);
         digitalWrite(RED_LED, LOW);
         digitalWrite(YELLOW_LED, LOW); //SPENGO ENTRAMBI I LED PRINCIPALI 
-        delay(5000);
         digitalWrite(LED_BUILTIN,LOW);
         return;
   }
   
-  wait +=1000;
   Serial.println(wait);
   digitalWrite(RED_LED, HIGH);
   digitalWrite(YELLOW_LED, LOW);
@@ -41,4 +42,15 @@ void loop() {
   digitalWrite(YELLOW_LED, HIGH);    
   delay(wait);                       
 
+}
+
+void blink(int led, int ritardo, int durata) {
+  int timeleft = durata;
+  while(timeleft>0) {
+    digitalWrite(led,HIGH);
+    delay(ritardo);
+    digitalWrite(led,LOW);
+    delay(ritardo);
+    timeleft -= ritardo*2;
+    }
 }
