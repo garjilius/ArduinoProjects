3dc88cf6cb452f4b0b3747458eac2ee24c596708
garjilius <emanuelegar@gmail.com>
2019-10-07 09:57:34 +0200

    Happy Birthday by Piezo

--- LCDFreenove/LCDFreenove.ino
+++ LCDFreenove/LCDFreenove.ino
@@ -9,7 +9,7 @@
 #include <LiquidCrystal.h>
 
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(11, 10, 5, 4, 3, 2);
 
 void setup() {
   // set up the LCD's number of columns and rows:
