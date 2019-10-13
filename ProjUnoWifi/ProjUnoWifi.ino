9a51f8d7b808c9e4ba40920c52a136f828171f31
garjilius <emanuelegar@gmail.com>
2019-10-13 16:38:15 +0200

    Bozza versione proj per UnoWifi

--- ProjUnoWifi/ProjUnoWifi.ino
+++ ProjUnoWifi/ProjUnoWifi.ino
@@ -1,37 +1,43 @@
-
-#include <SoftwareSerial.h>
-#include <BlynkSimpleStream.h>
 #include <DHT.h>
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <BlynkSimpleWiFiNINA.h>
 
 #define DHTPIN 2
-#define BLYNK_PRINT SwSerial
-#define EVHUM 0
-#define EVTEMP 1
-#define EVMOV 2
-#define HUMLIMIT 75
-#define TEMPLIMIT 25
 #define IRPIN 12
 #define MOVLED 13
 #define SYSLED 11
 
+#define BLYNK_PRINT Serial
+
+//La posizione degli elementi del'array notifiche relativi ai vari eventi
+#define EVHUM 0
+#define EVTEMP 1
+#define EVMOV 2
+
+#define HUMLIMIT 75
+#define TEMPLIMIT 25
+
+//Forse mi servirà un nuovo token
 char auth[] = "KjRZz0ewLqAP38p2kX6_TqnLXuWoYumK";
 unsigned long lastNotification;
 unsigned long currentMillis;
 const unsigned long delayNotificationMillis = 60000;
-bool notificationAllowed[5] = {true, true, true, true, true};
+bool notificationAllowed[3] = {true, true, true};
 bool systemDisabled = false;
 
-SoftwareSerial SwSerial(10, 11); // RX, TX
 WidgetTerminal terminal(V1);
 
 
 // Uncomment whatever type you're using!
-#define DHTTYPE DHT11     // DHT 11
-//#define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
+//#define DHTTYPE DHT11     // DHT 11
+#define DHTTYPE DHT22   // DHT 22, AM2302, AM2321 <--- Tipo del lab
 //#define DHTTYPE DHT21   // DHT 21, AM2301
 
 DHT dht(DHTPIN, DHTTYPE);
 BlynkTimer timer;
+char ssid[] = "***REMOVED***";
+char pass[] = "***REMOVED***";
 
 void sendSensor()
 {
@@ -41,11 +47,10 @@ void sendSensor()
   float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
 
   if (isnan(h) || isnan(t)) {
-    SwSerial.println("Failed to read from DHT sensor!");
+    Serial.println("Failed to read from DHT sensor!");
     return;
   }
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
+
   Blynk.virtualWrite(V5, h);
   Blynk.virtualWrite(V6, t);
 
@@ -104,16 +109,12 @@ void sendSensor()
 
 void setup()
 {
-  // Debug console
-  SwSerial.begin(9600);
-  lastNotification = millis();
-
-  // Blynk will work through Serial
-  // Do not read or write this serial manually in your sketch
   Serial.begin(9600);
-  Blynk.begin(Serial, auth);
-  pinMode(13, OUTPUT);
+  Blynk.begin(auth, ssid, pass);
 
+  lastNotification = millis();
+  pinMode(MOVLED, OUTPUT);
+  pinMode(SYSLED, OUTPUT);
   dht.begin();
 
   // Setup a function to be called every second
