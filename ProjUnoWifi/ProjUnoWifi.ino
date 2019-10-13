0d9dec3292c9ecc26503230d3d418e349303132c
garjilius <emanuelegar@gmail.com>
2019-10-13 19:08:44 +0200

    Sync dell'app col sistema dopo disconnessioni

--- ProjUnoWifi/ProjUnoWifi.ino
+++ ProjUnoWifi/ProjUnoWifi.ino
@@ -39,6 +39,11 @@ BlynkTimer timer;
 char ssid[] = "***REMOVED***";
 char pass[] = "***REMOVED***";
 
+BLYNK_CONNECTED() {
+  // Request Blynk server to re-send latest values for all pins
+  Blynk.syncAll();
+}
+
 void sendSensor()
 {
   terminal.flush(); //mi assicuro che il terminale non arrivi spezzettato
