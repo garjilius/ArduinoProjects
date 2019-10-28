d186f03be040bd44738cec5525992c73b4dfc1d5
garjilius <emanuelegar@gmail.com>
2019-10-28 16:19:44 +0100

    Pulizia

--- ProjUnoWifi/arduino_secrets.h
+++
@@ -1,6 +0,0 @@
-//#define SECRET_SSID "***REMOVED***"
-#define SECRET_SSID "***REMOVED***"
-#define SECRET_PASS "***REMOVED***"
-
-#define GoogleAppScript_ID "AKfycbyJgvc9Kg3UzkkN_IDy4rPSexJGnunSMjsVoP5gS6J2tvXId6MM"   // Google App Script id
-#define BlynkToken "QGJM5LWaUibrTKblRJ-EGO3dllEngTD1" //Blynk Token
